2486cc8784d07704db934df2a5f5c79e04fec71e
s25670-pj <92204700+s25670-pj@users.noreply.github.com>
2022-04-01 22:02:28 +0200

    Update S25670.java

--- zjazd2/S25670.java
+++ zjazd2/S25670.java
@@ -1,17 +1,27 @@
-public class S25670 extends Unit {
-			public S25670(final String id, final Pair position, final double rotate,
-					final CollisionModel model) {
-				super(id, position, rotate, model);
-				// TODO 
-			}
-			@Override
-			public void run() {
-				this.enableStopOnWall();
-				this.enableMovement();
-				while (true) {
-					if (whatIsInRange() == 3) {
-						this.attackInNextMove();
-					}
-				}
+package gamers;
+import main.Pair;
+import main.Unit;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class S25670 extends Unit
+{
+	public S25670(final String id, final Pair position, final double rotate, final CollisionModel model)
+	{
+		super(id, position, rotate, model);
+	}
+	@Override
+	public void run() 
+	{
+		this.enableStopOnWall();
+		this.enableMovement();
+		this.forward();
+				
+		while (true) 
+		{
+			if (whatIsInRange() == 3) 
+			{
+				this.attackInNextMove();
 			}
 		}
+	}
+}
